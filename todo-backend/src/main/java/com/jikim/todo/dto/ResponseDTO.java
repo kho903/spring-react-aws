a74db32dfac72348417c688722d94fd2379e2b87
Kim Ji Hun <gmldnr2222@naver.com>
2023-07-29 16:20:44 +0900

    feat: add ResponseDTO for HTTP Response

---
+++ todo-backend/src/main/java/com/jikim/todo/dto/ResponseDTO.java
@@ -0,0 +1,19 @@
+package com.jikim.todo.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@ToString
+public class ResponseDTO<T> {
+	private String error;
+	private List<T> data;
+}
