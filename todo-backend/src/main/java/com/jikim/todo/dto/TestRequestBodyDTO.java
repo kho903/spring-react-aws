e1c853d1a9c08d00c16f15cd47c1ac9866d6c83a
Kim Ji Hun <gmldnr2222@naver.com>
2023-07-29 17:38:52 +0900

    feat: add TestController for test
    - GetMapping
    - PathVariable
    - RequestParam
    - RequestBody
    - ResponseBody
    - ResponseEntity

---
+++ todo-backend/src/main/java/com/jikim/todo/dto/TestRequestBodyDTO.java
@@ -0,0 +1,14 @@
+package com.jikim.todo.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class TestRequestBodyDTO {
+
+	private int id;
+	private String message;
+}
