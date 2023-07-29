52bd7289238b4ebb65aea5f55b051514feb900ec
Kim Ji Hun <gmldnr2222@naver.com>
2023-07-29 16:47:46 +0900

    fix: id Long to String

--- todo-backend/src/main/java/com/jikim/todo/dto/TodoDTO.java
+++ todo-backend/src/main/java/com/jikim/todo/dto/TodoDTO.java
@@ -14,7 +14,7 @@ import lombok.ToString;
 @Getter
 @ToString
 public class TodoDTO {
-	private Long id;
+	private String id;
 	private String title;
 	private boolean done;
 
--- todo-backend/src/main/java/com/jikim/todo/model/TodoEntity.java
+++ todo-backend/src/main/java/com/jikim/todo/model/TodoEntity.java
@@ -14,7 +14,7 @@ import lombok.ToString;
 @Getter
 @ToString
 public class TodoEntity {
-	private Long id;
+	private String id;
 	private String userId;
 	private String title;
 	private boolean done;
