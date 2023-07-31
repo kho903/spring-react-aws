2390af04c790c917445f13576e92f734f2543f89
Kim Ji Hun <gmldnr2222@naver.com>
2023-07-31 22:22:38 +0900

    feat: Create Todo
    - TodoController - createTodo()
    - TodoEntity, TodoDto - add setter

--- todo-backend/src/main/java/com/jikim/todo/controller/TodoController.java
+++ todo-backend/src/main/java/com/jikim/todo/controller/TodoController.java
@@ -2,13 +2,18 @@ package com.jikim.todo.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jikim.todo.dto.ResponseDTO;
+import com.jikim.todo.dto.TodoDTO;
+import com.jikim.todo.model.TodoEntity;
 import com.jikim.todo.service.TodoService;
 
 @RestController
@@ -28,5 +33,34 @@ public class TodoController {
 		list.add(str);
 		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
 		return ResponseEntity.ok().body(response);
+	}
+
+	@PostMapping
+	public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto) {
+		try {
+
+			String temporaryUserId = "temporary-user"; // temporary user id.
+			TodoEntity entity = TodoDTO.toEntity(dto);
+			entity.setId(null);
+
+			entity.setUserId(temporaryUserId);
+
+			List<TodoEntity> entities = todoService.createTodo(entity);
+
+			List<TodoDTO> dtos = entities.stream()
+				.map(TodoDTO::new)
+				.collect(Collectors.toList());
+
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder()
+				.data(dtos)
+				.build();
+			return ResponseEntity.ok().body(response);
+		} catch (Exception e) {
+			String error = e.getMessage();
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder()
+				.error(error)
+				.build();
+			return ResponseEntity.badRequest().body(response);
+		}
 	}
 }
--- todo-backend/src/main/java/com/jikim/todo/model/TodoEntity.java
+++ todo-backend/src/main/java/com/jikim/todo/model/TodoEntity.java
@@ -11,12 +11,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 @ToString
 @Entity
 @Table(name = "Todo")
